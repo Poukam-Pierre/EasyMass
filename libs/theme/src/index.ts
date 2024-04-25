5686fade99c2cb718b14cd1c9414573073155e92
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-04-26 00:12:17 +0100

    feat(app): #2 export all necessary libraries from libs

--- libs/theme/src/index.ts
+++ libs/theme/src/index.ts
@@ -1,1 +1,6 @@
+export * from './EasyMesseThemeProvider';
+export * from './contexts/language/languageContextProvider';
+export * from './contexts/themeContext/AppThemeContextProvider';
+export * from './languages/en-us';
+export * from './languages/fr';
 export * from './theme';
