e9820afd9f0d7f3d5a04e289550cb654479b4929
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-04-28 10:37:42 +0100

    feat(app): #4 set up index layout for all page

---
+++ apps/easy-messe/pages/index.tsx
@@ -0,0 +1,26 @@
+import { useLanguage } from '@easy-messe/libs/theme';
+import { Box, Button, Typography } from '@mui/material';
+import { useIntl } from 'react-intl';
+
+export function Index() {
+    const { formatMessage } = useIntl();
+    const { activeLanguage, languageDispatch } = useLanguage();
+    return (
+        <Box>
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={() =>
+                    languageDispatch({
+                        type: activeLanguage === 'en' ? 'USE_FRENCH' : 'USE_ENGLISH',
+                    })
+                }
+            >
+                Change Language: {activeLanguage}
+            </Button>
+            <Typography variant="h1"> {formatMessage({ id: 'Hello' })}</Typography>
+        </Box>
+    );
+}
+
+export default Index;
