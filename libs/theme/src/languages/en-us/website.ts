d0917e6c3f0069d0563b0995fa495a33b8caa4f1
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-05-02 17:32:09 +0100

    feat(app): #6 add corresponding translation terms

--- libs/theme/src/languages/en-us/website.ts
+++ libs/theme/src/languages/en-us/website.ts
@@ -2,4 +2,8 @@ export const website = {
   home: 'Home',
   aboutUs: 'About',
   orderMass: 'Order Mass',
+  partners: 'Our Partners',
+  getInvolved: 'Get Involved',
+  footerMessage: 'Waiting for translations',
+  getInTooch: 'Get in tooch',
 };
