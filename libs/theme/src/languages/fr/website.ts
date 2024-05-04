b7f5750466bbed634107272c1c030943d6b0e442
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-05-04 20:11:46 +0100

    feat(app): #7 add translation library for hero section

--- libs/theme/src/languages/fr/website.ts
+++ libs/theme/src/languages/fr/website.ts
@@ -3,8 +3,12 @@ export const website = {
   aboutUs: 'A propos',
   orderMass: 'Offrir une messe',
   partners: 'Nos partenaires',
-  getInvolved: 'Devenir Assoocié',
+  getInvolved: 'Devenir Associé',
   footerMessage:
     "EasyMesse est la première platforme de demande de messes dans tout le térritoire Camerounais.Il a pour objectif de faciliter la vie aux fidèles et à l'Eglise dans leurs tâches.",
   getInTooch: 'Nous contacter',
+  heroMessage:
+    'Croire au saint sacrifice, Rendre grâce au Seigneur, Aimer nos proches disparus.',
+  subtitleHeroMessage:
+    "Demander qu'une messe soit célébrée à une intension est un acte de fois en la puissance du sacrifice du christ. C'est remettre entre les mains de Jesus les personnes vivantes et défunts que nous aimons ainsi que toutes nos intensions.",
 };
