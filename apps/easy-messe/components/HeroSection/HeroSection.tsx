15ed44c4e83a26a4a0de2c79374c2c9ced87880f
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-05-06 17:40:28 +0100

    feat(app): #7 made the hero function responsive

--- apps/easy-messe/components/HeroSection/HeroSection.tsx
+++ apps/easy-messe/components/HeroSection/HeroSection.tsx
@@ -6,35 +6,50 @@ import { useIntl } from "react-intl";
 export default function HeroSection() {
     const { formatMessage } = useIntl()
     return (
-        <Box sx={{
-            display: 'grid',
-            gridTemplateColumns: '1fr auto',
-            padding: '0px 90px'
-
-        }}>
-            <Box sx={{
+        <Box
+            component='section'
+            sx={{
                 display: 'grid',
-                alignItems: 'end',
-                paddingTop: '130px',
-                rowGap: '35px'
-            }}>
-                <Typography sx={{
-                    fontSize: '56px',
-                    lineHeight: '60px',
-                    paragraphSpacing: '20px',
-                    fontWeight: 700,
-                    color: theme.common.titleActive,
-                    width: '900px'
-                }}>
+                gridTemplateColumns: {
+                    laptop: '1fr 1.4fr',
+                    tablet: '1fr 1.2fr',
+                    mobile: 'none'
+                },
+                padding: { laptop: '0px 90px', mobile: '0px 21px' },
+                position: 'relative',
+                rowGap: '5px'
+            }}
+        >
+            <Box
+                sx={{
+                    display: 'grid',
+                    alignItems: 'end',
+                    paddingTop: { laptop: '130px', mobile: '30px' },
+                    rowGap: '35px'
+                }}
+            >
+                <Typography
+                    variant="h1"
+                    sx={{
+                        fontSize: { laptop: '56px', tablet: '42px', mobile: '30px' },
+                        lineHeight: { laptop: '60px', tablet: '54px', mobile: '36px' },
+                        fontWeight: { laptop: 800, mobile: 700 },
+                        color: theme.common.titleActive,
+                        width: { laptop: '900px', tablet: 'auto', },
+                        padding: { laptop: 'initial', mobile: 0 },
+                        letterSpacing: '-0.0em'
+                    }}>
                     {formatMessage({ id: 'heroMessage' })}
                 </Typography>
-                <Typography variant="h4" sx={{
-                    lineHeight: '28px',
-                    letterSpacing: 0,
-                    paddingBottom: 0,
-                    fontWeight: 700,
-                    width: '750px'
-                }}>
+                <Typography
+                    variant="h4"
+                    sx={{
+                        lineHeight: '28px',
+                        letterSpacing: { laptop: 0, mobile: '-0.02em' },
+                        paddingBottom: 0,
+                        fontWeight: 700,
+                        width: { laptop: '750px', tablet: 'auto' },
+                    }}>
                     {formatMessage({ id: 'subtitleHeroMessage' })}
                 </Typography>
                 <Button
@@ -45,7 +60,6 @@ export default function HeroSection() {
                     }}
                 >
                     {formatMessage({ id: 'orderMass' })}
-
                 </Button>
             </Box>
             <HeroImage />
