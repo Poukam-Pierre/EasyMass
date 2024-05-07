ac5247122bf2cac48e973e609e739c9de0849ab4
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-05-07 14:38:44 +0100

    feat(app): #9 add aboutUs component to index view port

--- apps/easy-messe/pages/index.tsx
+++ apps/easy-messe/pages/index.tsx
@@ -1,9 +1,11 @@
+import AboutUs from '../components/AboutUs/AboutUs';
 import HeroSection from '../components/HeroSection/HeroSection';
 
 export function Index() {
     return (
         <>
             <HeroSection />
+            <AboutUs />
         </>
     );
 }
