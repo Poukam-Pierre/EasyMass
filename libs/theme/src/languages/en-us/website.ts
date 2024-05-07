40edbed71a757b89484be053db4b5a1c8f544b5b
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-05-07 15:02:38 +0100

    feat(app): #9 add intl library

--- libs/theme/src/languages/en-us/website.ts
+++ libs/theme/src/languages/en-us/website.ts
@@ -1,6 +1,10 @@
 export const website = {
   home: 'Home',
   aboutUs: 'About',
+  subtitleAboutUs: 'Welcome to the EasyMesse portal',
+  masses: 'Masses',
+  faithfull: 'Faithfulls',
+  parish: 'Parishes',
   orderMass: 'Order Mass',
   partners: 'Our Partners',
   getInvolved: 'Get Involved',
@@ -11,4 +15,6 @@ export const website = {
     'Believe in the holy sacrifice, To give thanks to the Lord, Loving our departed loved ones.',
   subtitleHeroMessage:
     "Asking for a Mass to be celebrated for an intention is an act of faith in the power of Christ's sacrifice. It means placing in the hands of Jesus the living and deceased people we love, as well as all our intentions.",
+  aboutUsMessage:
+    'EasyMesse is a platform that allows you to quickly request a mass across the entire national and international territory. Connected to almost every parish in the country and elsewhere, you can offer a mass of your choice according to your intentions and in the parish of your choice. Trusting us means that you can request a mass from anywhere up to 1 hour before the mass and be sure that your request will be fulfilled.',
 };
