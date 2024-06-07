6d536f98bd3f9267acadfa4af28c2c34c073f59f
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-06-07 20:49:05 +0100

    feat(app): #15 set up offer list function component.

---
+++ apps/easy-messe/components/OfferMass/OfferList.tsx
@@ -0,0 +1,25 @@
+import { Box, Button, Typography } from "@mui/material";
+
+interface OfferListProps {
+    children: JSX.Element
+}
+
+export default function OfferList({ children }: OfferListProps) {
+    return (
+        <Box sx={{
+            padding: '10px 10px 0px 10px',
+            display: 'grid',
+            gridTemplateRows: 'auto 1fr auto'
+        }}>
+            <Typography variant="h2">Liste de demandes</Typography>
+            {children}
+            <Button
+                variant="contained"
+                disableElevation
+                sx={{
+                    width: 'fit-content'
+                }}
+            >Payer</Button>
+        </Box>
+    );
+}
