29f4793ff4549409e4ffd43c3f9723fd1ac50c4c
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-06-11 14:09:34 +0100

    feat(app): #15 add tab for mobile view.

--- apps/easy-messe/pages/offer-mass/index.tsx
+++ apps/easy-messe/pages/offer-mass/index.tsx
@@ -1,16 +1,42 @@
-import { Box, Divider, Typography } from "@mui/material";
+import { Box, Divider, Tabs, Typography, Tab } from "@mui/material";
 import HeroMass from "../../components/OfferMass/HeroMass";
 import LetOfferMass from "../../components/OfferMass/LetOfferMass";
 import OfferList from "apps/easy-messe/components/OfferMass/OfferList";
 import warningIcon from '@iconify-icons/fluent/warning-24-regular'
 import { Icon } from "@iconify/react";
+import { ReactNode, useState } from "react";
+import { theme } from "@easy-messe/libs/theme";
 
+type TabComponent = Record<number, ReactNode>;
 export default function OrderMass() {
+    const [activeTabIndex, setActiveTabIndex] = useState<number>(0)
+    const tabTitle = ['Liste de demande', 'Demande de messes']
+
+    const tabComponent: TabComponent = {
+        0: (
+            <OfferList>
+                <Box sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}>
+                    <Icon icon={warningIcon} fontSize={24} />
+                    <Typography variant='body2'>Aucune demande de messe en cours</Typography>
+                </Box>
+            </OfferList>
+        ),
+        1: (
+            <LetOfferMass />
+        )
+    }
+
     return (
         <>
             <HeroMass />
             <Box sx={{
-                padding: '40px 90px'
+                padding: '40px 90px',
+                display: { laptop: 'inherit', mobile: 'none' }
             }}>
                 <Typography variant="h2"> Demande de messe</Typography>
                 <Divider />
@@ -32,6 +58,28 @@ export default function OrderMass() {
                         </Box>
                     </OfferList>
                 </Box>
+            </Box>
+            <Box sx={{
+                display: { laptop: 'none', mobile: 'inherit' }
+            }}>
+                <Tabs
+                    value={activeTabIndex}
+                    onChange={(_, tabIndex) => setActiveTabIndex(tabIndex)}
+                    textColor='primary'
+                    indicatorColor="primary"
+                    centered
+                >
+                    {
+                        tabTitle.map((tabTitle, index) => (
+                            <Tab
+                                disableRipple
+                                key={index}
+                                label={tabTitle}
+                            />
+                        ))
+                    }
+                </Tabs>
+                {tabComponent[activeTabIndex]}
             </Box>
         </>
     );
