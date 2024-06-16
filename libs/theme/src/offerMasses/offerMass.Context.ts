9964cbed5fe1e7a54dc8b8eed1e5770d14e387f3
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-06-16 20:11:51 +0100

    feat(app): #15 set up offer mass context hook.

--- libs/theme/src/offerMasses/offerMass.Context.ts
+++ libs/theme/src/offerMasses/offerMass.Context.ts
@@ -2,20 +2,6 @@ import { createContext } from 'react';
 import { MassRequested } from './offerMass.interface';
 
 export const MassOfferContext = createContext<MassRequested>({
-  massRequested: {
-    faithInfos: {
-      name: '',
-      phone: '',
-      anonymous: false,
-    },
-    massInfos: [
-      {
-        city: '',
-        parish: '',
-        dateTime: null,
-        intension: '',
-      },
-    ],
-  },
+  massRequested: null,
   massRequestDispatch: () => null,
 });
