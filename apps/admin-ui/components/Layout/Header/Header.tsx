b0a68c9377ab9b8ea64eb226cf1e645eb986b55f
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-07-04 20:26:49 +0100

    feat(app): #22 set up header of app.

---
+++ apps/admin-ui/components/Layout/Header/Header.tsx
@@ -0,0 +1,58 @@
+import { Box, Divider } from "@mui/material";
+import Breadcrumb from "./Breadcrumbs";
+import { useRouter } from "next/router";
+import LanguageSwapper from "../LanguageSwapper";
+
+export interface BreadcrumbsNameMaps {
+    title: string;
+    links: { [key: string]: string }
+}
+export default function Header() {
+    const { pathname } = useRouter()
+
+    const breadcrumbsNameMap: BreadcrumbsNameMaps[] = [
+        {
+            title: 'Mass Offer',
+            links: {
+                '/massOffer': 'Mass offer',
+            }
+        },
+        {
+            title: 'Masses',
+            links: {
+                '/masses': 'Masses',
+                '/masses/create': 'Create',
+                '/masses/modify': 'Modify',
+                '/masses/delete': 'Cancel',
+            }
+        },
+        {
+            title: 'Finances',
+            links: {
+                '/finances': 'Finances',
+                'finances/widrawal': 'Widrawal',
+            }
+        },
+    ]
+    return (
+        <Box sx={{
+            display: 'grid',
+            rowGap: 1,
+            padding: '0px 50px'
+        }}>
+            <Box sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+            }}>
+                <Breadcrumb breadcrumbs={
+                    breadcrumbsNameMap.find((BreadcrumbsNameMaps) =>
+                        BreadcrumbsNameMaps.links[pathname] !== undefined
+                    ) as BreadcrumbsNameMaps
+                } />
+                <LanguageSwapper />
+            </Box>
+            <Divider />
+        </Box>
+    );
+}
