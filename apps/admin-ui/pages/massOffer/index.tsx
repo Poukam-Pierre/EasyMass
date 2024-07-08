627e50f201a6b7e52fcdf1cdb89f50dbe2644edb
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-07-08 17:45:00 +0100

    feat(app):#28 update page of offer mass.

--- apps/admin-ui/pages/massOffer/index.tsx
+++ apps/admin-ui/pages/massOffer/index.tsx
@@ -1,9 +1,55 @@
+import { Box, Typography } from "@mui/material";
 import AppLayout from "../../components/Layout";
+import MassOfferTable from "../../components/MassOffer/MassofferTable";
+import filterIcon from '@iconify-icons/fluent/filter-24-regular'
+import searchIcon from '@iconify-icons/fluent/search-24-regular'
+import { Icon } from "@iconify/react";
+
+
 
 export default function MassOrder() {
     return (
         <AppLayout>
-            Hello table of masses ordered!
+            <Box>
+                <Box sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                    padding: '0 16px',
+                    marginBottom: 1
+
+                }}>
+                    <Typography
+                        variant='h3'
+                        color='primary'
+                    >
+                        Liste des différentes offres de messe
+                    </Typography>
+                    <Box sx={{
+                        display: 'grid',
+                        gridTemplateColumns: 'auto 1fr',
+                        alignItems: 'center',
+                        columnGap: 1
+                    }}>
+                        <Icon icon={searchIcon} fontSize={20} />
+                        <Typography
+                            variant='body2'
+                        >Recherche</Typography>
+                    </Box>
+                    <Box sx={{
+                        display: 'grid',
+                        gridTemplateColumns: 'auto 1fr',
+                        alignItems: 'center',
+                        columnGap: 1
+                    }}>
+                        <Icon icon={filterIcon} fontSize={20} />
+                        <Typography
+                            variant='body2'
+                        >Filtres</Typography>
+                    </Box>
+                </Box>
+                <MassOfferTable />
+            </Box>
         </AppLayout>
     );
 }
