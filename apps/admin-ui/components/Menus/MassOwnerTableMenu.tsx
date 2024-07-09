adce16795ade0b23629bcd065f372e2a87d07cbc
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-07-09 13:35:47 +0100

    feat(app):#30 set up menu for each row.

---
+++ apps/admin-ui/components/Menus/MassOwnerTableMenu.tsx
@@ -0,0 +1,72 @@
+import { Box, Menu, MenuItem, Typography } from "@mui/material";
+import trashIcon from '@iconify-icons/ph/trash-light'
+import editIcon from '@iconify-icons/fluent/edit-28-regular'
+import { Icon } from "@iconify/react";
+import { MenuIntem } from "./MassMenu";
+
+interface MenuItemForMassOwner extends MenuIntem {
+    color?: string
+}
+
+export default function MassOwnerTableMenu({
+    anchorEl,
+    setAnchorEl,
+}: {
+    anchorEl: HTMLElement | null;
+    setAnchorEl: (anchor: HTMLElement | null) => void;
+}) {
+    const menuItem: MenuItemForMassOwner[] = [
+        {
+            title: 'Modify',
+            icon: editIcon
+        },
+        {
+            title: 'Delete',
+            icon: trashIcon,
+            color: 'var(--error)'
+        },
+    ]
+    return (
+        <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={() => setAnchorEl(null)}
+            anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'center',
+            }}
+            transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+        >
+            {menuItem.map(({ title, icon, color }, index) => (
+                <MenuItem
+                    key={index}
+                    value={title}
+                >
+                    <Box sx={{
+                        display: 'grid',
+                        gridTemplateColumns: 'auto 1fr',
+                        columnGap: 1,
+                        alignItems: 'center',
+                        color: color,
+                    }}>
+                        <Icon
+                            icon={icon}
+                            fontSize={16}
+                        />
+                        <Typography
+                            variant="body1"
+                            sx={{
+                                fontWeight: 500,
+                            }}
+                        >
+                            {title}
+                        </Typography>
+                    </Box>
+                </MenuItem>
+            ))}
+        </Menu>
+    );
+}
