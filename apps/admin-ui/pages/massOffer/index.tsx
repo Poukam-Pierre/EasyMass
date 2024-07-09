5a7c263b641082ea8227291fd6d30aaf70261779
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-07-09 11:05:36 +0100

    refactor(app): #28 format code.

--- apps/admin-ui/pages/massOffer/index.tsx
+++ apps/admin-ui/pages/massOffer/index.tsx
@@ -1,72 +1,70 @@
-import { Box, InputAdornment, InputBase, TextField, Typography } from "@mui/material";
+import filterIcon from '@iconify-icons/fluent/filter-24-regular';
+import searchIcon from '@iconify-icons/fluent/search-24-regular';
+import { Icon } from "@iconify/react";
+import { Box, InputBase, Typography } from "@mui/material";
+import { useState } from "react";
+import { useIntl } from "react-intl";
 import AppLayout from "../../components/Layout";
 import MassOfferTable from "../../components/MassOffer/MassofferTable";
-import filterIcon from '@iconify-icons/fluent/filter-24-regular'
-import searchIcon from '@iconify-icons/fluent/search-24-regular'
-import { Icon } from "@iconify/react";
-import { useState } from "react";
 import MassOfferMenu from "../../components/Menus/MassOfferMenu";
-import { useIntl } from "react-intl";
 
 
 
-export default function MassOrder() {
+export default function MassOffer() {
     const { formatMessage } = useIntl()
     const [anchorEl, setAnchorEl] = useState<HTMLAnchorElement | null>(null);
     return (
         <AppLayout>
-            <Box>
-                <MassOfferMenu
-                    anchorEl={anchorEl}
-                    setAnchorEl={setAnchorEl}
-                />
-                <Box sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'space-between',
-                    padding: '0 16px',
-                    marginBottom: 1
+            <MassOfferMenu
+                anchorEl={anchorEl}
+                setAnchorEl={setAnchorEl}
+            />
+            <Box sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                padding: '0 16px',
+                marginBottom: 1
 
-                }}>
-                    <Typography
-                        variant='h3'
-                        color='primary'
-                        sx={{
-                            paddingBottom: 0
-                        }}
-                    >
-                        {formatMessage({ id: 'listOfMassSupply' })}
-                    </Typography>
-                    <Box sx={{
-                        display: 'grid',
-                        gridTemplateColumns: 'auto 1fr',
-                        alignItems: 'center',
-                        columnGap: 1
-                    }}>
-                        <Icon icon={searchIcon} fontSize={20} />
-                        <InputBase
-                            placeholder={formatMessage({ id: 'search' })}
-                        />
-                    </Box>
-                    <Box sx={{
-                        display: 'grid',
-                        gridTemplateColumns: 'auto 1fr',
-                        alignItems: 'center',
-                        columnGap: 1,
-                        cursor: 'pointer',
+            }}>
+                <Typography
+                    variant='h3'
+                    color='primary'
+                    sx={{
+                        paddingBottom: 0
                     }}
-                        onClick={(event) => setAnchorEl(event.target as HTMLAnchorElement)}
-                    >
-                        <Icon icon={filterIcon} fontSize={20} />
-                        <Typography
-                            variant='body2'
-                        >
-                            {formatMessage({ id: 'filter' })}
-                        </Typography>
-                    </Box>
+                >
+                    {formatMessage({ id: 'listOfMassSupply' })}
+                </Typography>
+                <Box sx={{
+                    display: 'grid',
+                    gridTemplateColumns: 'auto 1fr',
+                    alignItems: 'center',
+                    columnGap: 1
+                }}>
+                    <Icon icon={searchIcon} fontSize={20} />
+                    <InputBase
+                        placeholder={formatMessage({ id: 'search' })}
+                    />
                 </Box>
-                <MassOfferTable />
+                <Box sx={{
+                    display: 'grid',
+                    gridTemplateColumns: 'auto 1fr',
+                    alignItems: 'center',
+                    columnGap: 1,
+                    cursor: 'pointer',
+                }}
+                    onClick={(event) => setAnchorEl(event.target as HTMLAnchorElement)}
+                >
+                    <Icon icon={filterIcon} fontSize={20} />
+                    <Typography
+                        variant='body2'
+                    >
+                        {formatMessage({ id: 'filter' })}
+                    </Typography>
+                </Box>
             </Box>
+            <MassOfferTable />
         </AppLayout>
     );
 }
