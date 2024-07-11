6f7d66eee341b1bd4f90f12817852548b6797639
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-07-11 13:33:02 +0100

    feat(app):#19 set up general admin layout.

--- libs/shared-ui/src/index.ts
+++ libs/shared-ui/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/styles/index';
+export * from './lib/adminLayout/AdminLayout';
---
+++ libs/shared-ui/src/lib/adminLayout/AdminLayout.tsx
@@ -0,0 +1,22 @@
+import { EasyMassThemeProvider, useLanguage } from "@easy-messe/libs/theme";
+import { Box } from "@mui/material";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import Head from "next/head";
+import { PropsWithChildren } from "react";
+
+export function EasyMassAdminLayout({ children }: PropsWithChildren) {
+    const { activeLanguage } = useLanguage()
+    return (
+        <EasyMassThemeProvider defaultLang="fr">
+            <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={activeLanguage}>
+                <Head>
+                    <title>{"EasyMesse"}</title>
+                </Head>
+                <Box component="main" className="app">
+                    {children}
+                </Box>
+            </LocalizationProvider>
+        </EasyMassThemeProvider>
+    );
+}
