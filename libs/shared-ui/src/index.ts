5833972572e093e2f87ad4185c697011d698d7f5
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-07-14 14:52:34 +0100

    feat(app):#31 import functions utils from library.

--- libs/shared-ui/src/index.ts
+++ libs/shared-ui/src/index.ts
@@ -1,2 +1,6 @@
 export * from './lib/styles/index';
 export * from './lib/adminLayout/AdminLayout';
+export * from './lib/adminLayout/Header/Header';
+export * from './lib/adminLayout/SideBar/SideBar';
+export * from './lib/adminLayout/SideBar/Profile';
+export * from './lib/adminLayout/Footer';
