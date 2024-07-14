d5859c7cf13fb0f3a887dcb627820626fa923cc7
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-07-14 16:11:29 +0100

    feat(app):#31 add login function component, related to get login cretential, into UI library

---
+++ libs/shared-ui/src/lib/authentication/Login.tsx
@@ -0,0 +1,137 @@
+import invisibleIcon from '@iconify-icons/material-symbols/visibility-off-outline';
+import visibleIcon from '@iconify-icons/material-symbols/visibility-outline';
+import { Icon } from "@iconify/react";
+import {
+    Box,
+    Button,
+    FormControl,
+    FormHelperText,
+    IconButton,
+    InputAdornment,
+    OutlinedInput,
+    TextField
+} from "@mui/material";
+import { useFormik } from 'formik';
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { useIntl } from "react-intl";
+import * as yup from 'yup';
+import HeroHeader from "./HeroHeader";
+
+
+
+export function Login() {
+    const [isVisible, setIsVisible] = useState<boolean>(false)
+    const { formatMessage } = useIntl()
+    const { push } = useRouter()
+
+    const {
+        handleSubmit,
+        errors,
+        touched,
+        handleChange
+    } = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+        onSubmit: (values) => {
+            // TODO fetch data to API for authentication
+            console.log(values)
+        },
+        validationSchema: yup.object().shape({
+            email: yup.string().required(formatMessage({ id: 'Should provide email' })),
+            password: yup.string()
+                .min(5, formatMessage({ id: 'At least 5 caracters' }))
+                .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/, {
+                    message: formatMessage({ id: '1 magiscule, 1 minuscule et 1 chiffre' })
+                })
+                .required(formatMessage({ id: 'Should provide password' }))
+        }),
+        enableReinitialize: true
+    })
+    return (
+        <Box sx={{
+            width: 464,
+            display: 'grid',
+            height: 'fit-content',
+            marginTop: '70px',
+            justifySelf: 'center',
+            rowGap: 3
+        }}>
+            <HeroHeader
+                slogan={formatMessage({ id: 'slogan' })}
+                greeting={formatMessage({ id: 'greeting' })}
+                getActionMsg={formatMessage({ id: 'logInMsg' })}
+            />
+            <Box
+                sx={{
+                    display: 'grid',
+                    rowGap: 2
+                }}
+                component='form'
+                onSubmit={handleSubmit}
+            >
+                <TextField
+                    name="email"
+                    size="small"
+                    placeholder="Email"
+                    type="email"
+                    error={errors.email && touched.email ? true : false}
+                    helperText={(errors.email && touched.email) && errors.email}
+                    onChange={handleChange}
+                />
+                <FormControl
+                    variant="outlined"
+                    size='small'
+                >
+                    <OutlinedInput
+                        id="outlined-adornment-password"
+                        name="password"
+                        placeholder={formatMessage({ id: 'password' })}
+                        type={isVisible ? 'text' : 'password'}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    onClick={() => setIsVisible((val) => !val)}
+                                    edge="end"
+                                >
+                                    {isVisible ?
+                                        <Icon icon={visibleIcon} fontSize={24} /> :
+                                        <Icon icon={invisibleIcon} fontSize={24} />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                        sx={{
+                            '& .MuiOutlinedInput-notchedOutline': {
+                                borderRadius: '8px'
+                            }
+                        }}
+                        error={errors.password && touched.password ? true : false}
+                        onChange={handleChange}
+                    />
+                    {errors.password && touched.password ? (
+                        <FormHelperText error>{errors.password}</FormHelperText>
+                    ) : (
+                        ''
+                    )}
+
+                </FormControl>
+                <Button
+                    variant="contained"
+                    type="submit"
+                >
+                    {formatMessage({ id: 'connexion' })}
+                </Button>
+                <Button
+                    variant='text'
+                    disableRipple
+                    onClick={() => push('/recovery/verification')}
+                >
+                    {formatMessage({ id: 'forgotPassword' })}
+                </Button>
+            </Box>
+        </Box>
+
+    );
+}
