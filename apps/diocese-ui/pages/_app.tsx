67024ee1f7f0b69a9db1a2a6e41b41796229d9a1
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-07-14 19:31:02 +0100

    ci(app): #32 initialize diocese UI

---
+++ apps/diocese-ui/pages/_app.tsx
@@ -0,0 +1,24 @@
+import '@easy-messe/shared-ui';
+import { EasyMassAdminLayout } from '@easy-messe/shared-ui';
+import { AppProps } from 'next/app';
+
+type NextPageWithoutLayout = AppProps & {
+    Component: AppProps['Component'] & {
+        getLayout?: (page: React.ReactNode) => React.ReactNode;
+    };
+};
+
+function CustomApp({ Component, pageProps }: NextPageWithoutLayout) {
+    const getLayout = Component.getLayout ??
+        ((page) =>
+            <EasyMassAdminLayout>
+                {page}
+            </EasyMassAdminLayout>
+        );
+
+    return getLayout(
+        <Component {...pageProps} />
+    );
+}
+
+export default CustomApp;
---
+++ apps/diocese-ui/pages/index.tsx
@@ -0,0 +1,8 @@
+
+export function Index() {
+    return (
+        <div>Hello Diocese UI</div>
+    );
+}
+
+export default Index;
