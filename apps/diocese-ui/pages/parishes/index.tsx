384b49de5bcdccb2f29ecd6e648b05c3c1fa217c
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-07-23 17:21:32 +0100

    fix(app): #36 add id for each data.

--- apps/diocese-ui/pages/parishes/index.tsx
+++ apps/diocese-ui/pages/parishes/index.tsx
@@ -1,31 +1,32 @@
 import { EasyMassAdminLayout } from "@easy-messe/shared-ui";
 import { Box, Button, Typography } from "@mui/material";
 import { ReactNode, useEffect, useState } from "react";
-import { useIntl } from "react-intl";
 import AppLayout from "../../components/Layout";
 import ParishesTable, { ParishData } from "../../components/Parishes/ParishesTables";
 
 
 
 export default function Parishes() {
-    const { formatMessage } = useIntl()
     const [parishData, setParishData] = useState<ParishData[]>([])
 
 
     const parishesData: ParishData[] = [
         {
+            id: 1,
             name: 'Saint Paul Apôtre',
             city: 'Bangangté',
             email: 'saintp@gmail.com',
             contact: '+237 680 090 489'
         },
         {
+            id: 2,
             name: 'Marie Reine des apôtres de Kamtop',
-            city: 'Baafoussam',
+            city: 'Bafoussam',
             email: 'marier@gmail.com',
             contact: '+237 680 090 489'
         },
         {
+            id: 3,
             name: 'Notre dame des sept douleur de Bangangté',
             city: 'Bangangté',
             email: 'notred@gmail.com',
