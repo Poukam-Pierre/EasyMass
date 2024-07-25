0f6c1bba44129cee3f2868da097f9c0b9532e705
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-07-25 12:29:03 +0100

    feat(app):#37 set up financial table menu.

---
+++ apps/diocese-ui/components/Menu/FinancialTableMenu.tsx
@@ -0,0 +1,70 @@
+import { Icon, IconifyIcon } from "@iconify/react";
+import { Box, Menu, MenuItem, Typography } from "@mui/material";
+
+export interface MenuItem {
+    title: string;
+    icon: IconifyIcon
+}
+
+interface FinancialTableMenuProps {
+    anchorEl: HTMLElement | null;
+    setAnchorEl: (anchor: HTMLElement | null) => void;
+    menuItem: MenuItem[];
+    idSelected: number;
+}
+export default function FinancialTableMenu({
+    anchorEl,
+    setAnchorEl,
+    menuItem,
+    idSelected,
+}: FinancialTableMenuProps) {
+    const handleModalDialog = () => {
+        setAnchorEl(null)
+        console.log(idSelected)
+    }
+
+    return (
+        <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={() => setAnchorEl(null)}
+            anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'center',
+            }}
+            transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+        >
+            {menuItem.map(({ title, icon }, index) => (
+                <MenuItem
+                    key={index}
+                    value={title}
+                    onClick={() => handleModalDialog()}
+
+                >
+                    <Box sx={{
+                        display: 'grid',
+                        gridTemplateColumns: 'auto 1fr',
+                        columnGap: 1,
+                        alignItems: 'center',
+                    }}>
+                        <Icon
+                            icon={icon}
+                            fontSize={16}
+                        />
+                        <Typography
+                            variant="body1"
+                            sx={{
+                                fontWeight: 500,
+                            }}
+                        >
+                            {title}
+                        </Typography>
+                    </Box>
+                </MenuItem>
+            ))}
+        </Menu>
+    );
+}
