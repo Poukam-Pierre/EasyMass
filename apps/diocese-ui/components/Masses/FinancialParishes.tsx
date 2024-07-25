c0ea4502425bce4e1329eba63bee21bbc72ba870
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-07-25 11:28:47 +0100

    feat(app):#37 set up financial table related to parish's masses.

---
+++ apps/diocese-ui/components/Masses/FinancialParishes.tsx
@@ -0,0 +1,114 @@
+import { theme } from "@easy-messe/libs/theme";
+import { Icon } from "@iconify/react";
+import {
+    IconButton,
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableRow,
+    Typography
+} from "@mui/material";
+import { useIntl } from "react-intl";
+import verticalDotsIcon from '@iconify-icons/ph/dots-three-outline-vertical-fill';
+import { MouseEvent, useState } from "react";
+
+export interface FinanceParish {
+    id: number;
+    name: string;
+    city: string;
+    massType: string;
+    price: number;
+}
+
+export default function FinancialTableParishes({
+    financeParishData
+}: {
+    financeParishData: FinanceParish[]
+}) {
+    const { formatMessage, formatNumber } = useIntl();
+    const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+    const [idSelected, setIdSelected] = useState<number | undefined>();
+    const titles: string[] = ['parishes', 'city', 'massType', 'price', 'action'];
+
+
+    const handleActionOnRow = (event: MouseEvent<HTMLElement>, id: number) => {
+        setAnchorEl(event.currentTarget);
+        setIdSelected(id)
+    }
+
+    return (
+        <Table>
+            <TableHead>
+                <TableRow>
+                    {titles.map((title, index) => (
+                        <TableCell
+                            key={index}
+                            sx={{
+                                bgcolor: theme.palette.secondary.main,
+                                fontWeight: 600
+                            }}
+                        >
+                            {formatMessage({ id: title }).toUpperCase()}
+                        </TableCell>
+                    ))}
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {financeParishData.map(({
+                    id, name, city,
+                    massType, price
+                }, index) => (
+                    <TableRow
+                        key={`${index} + ${name}`}
+                        sx={{
+                            color: 'var(--label)'
+                        }}
+                    >
+                        <TableCell>
+                            <Typography sx={{
+                                width: '400px',
+                                textOverflow: "ellipsis",
+                                overflow: "hidden",
+                                whiteSpace: "nowrap",
+                                fontWeight: 600,
+                                color: 'var(--label)'
+                            }}>
+                                {name}
+                            </Typography>
+                        </TableCell>
+                        <TableCell sx={{
+                            fontWeight: 600,
+                            color: 'var(--label)'
+                        }}>
+                            {city}
+                        </TableCell>
+                        <TableCell sx={{
+                            fontWeight: 600,
+                            color: 'var(--label)'
+                        }}>{massType}</TableCell>
+                        <TableCell sx={{
+                            fontWeight: 600,
+                            color: 'var(--label)'
+                        }}>
+                            {formatNumber(price, {
+                                style: 'currency',
+                                currency: 'xaf'
+                            })}
+                        </TableCell>
+                        <TableCell align='right'>
+                            <IconButton
+                                size="small"
+                                onClick={(event) => handleActionOnRow(event, id)}
+                            >
+                                <Icon icon={verticalDotsIcon} fontSize={18} />
+                            </IconButton>
+                        </TableCell>
+
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
+
+    );
+}
