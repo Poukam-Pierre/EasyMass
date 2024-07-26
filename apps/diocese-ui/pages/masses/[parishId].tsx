f01dfb33b268e08cbf5f80cf21fd6bede23454b9
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-07-26 11:17:17 +0100

    feat(app):#37 Add statistics table for masses throught the time.

--- apps/diocese-ui/pages/masses/[parishId].tsx
+++ apps/diocese-ui/pages/masses/[parishId].tsx
@@ -6,21 +6,19 @@ import { useIntl } from "react-intl";
 import { Icon } from "@iconify/react";
 import editIcon from '@iconify-icons/fluent/edit-28-regular';
 import { useRouter } from "next/router";
+import MassesStatTable from "./MassesStatTable";
+import ParishesDialog from "../../components/Parishes/Dialog/Parishes";
+import { ParishData } from "../../components/Parishes/ParishesTables";
 
-interface ParishDataWithoutId {
-    name: string;
-    city: string;
-    email: string;
-    contact: string;
-    leadName: string;
-
-}
 export default function ParishOverview() {
     const staticData: string[] = ['name', 'city', 'Email', 'Responsable', 'phoneNumber']
     const { formatMessage } = useIntl()
     const [parishData, setParishData] = useState<ParishData>()
     const { query: { parishId } } = useRouter()
-    const parishinfo: ParishDataWithoutId = {
+    const [isOpenDialogModif, setIsOpenDialogModif] = useState<boolean>(false);
+
+    const parishinfo: ParishData = {
+        id: 1,
         name: 'Saint Paul Apôtre',
         city: 'Bangangté',
         email: 'saintp@gmail.com',
@@ -42,81 +40,83 @@ export default function ParishOverview() {
                 parishData={parishData}
             />
 
-        <Box sx={{
-            display: 'grid',
-            gap: 2
-        }}>
             <Box sx={{
                 display: 'grid',
-                gridAutoFlow: 'column',
-                alignItems: 'center',
-                justifyContent: "flex-start",
-                columnGap: 5
+                gap: 2
             }}>
-                <Typography
-                    variant="h4"
-                    color="primary"
-                    sx={{
-                        letterSpacing: 0,
-                        paddingBottom: 0,
-                    }}
+                <Box sx={{
+                    display: 'grid',
+                    gridAutoFlow: 'column',
+                    alignItems: 'center',
+                    justifyContent: "flex-start",
+                    columnGap: 5
+                }}>
+                    <Typography
+                        variant="h4"
+                        color="primary"
+                        sx={{
+                            letterSpacing: 0,
+                            paddingBottom: 0,
+                        }}
 
-                >
-                    Informations Personnelles
-                </Typography>
-                <Button
-                    startIcon={
-                        <Icon icon={editIcon} fontSize={10} />
-                    }
-                    variant="outlined"
-                    size="small"
-                    sx={{
-                        color: 'var(--body)',
-                        borderColor: 'var(--line)',
-                        borderRadius: '30px',
-                        padding: '5px'
-                    }}
+                    >
+                        Informations Personnelles
+                    </Typography>
+                    <Button
+                        startIcon={
+                            <Icon icon={editIcon} fontSize={10} />
+                        }
+                        variant="outlined"
+                        size="small"
+                        sx={{
+                            color: 'var(--body)',
+                            borderColor: 'var(--line)',
+                            borderRadius: '30px',
+                            padding: '5px'
+                        }}
                         onClick={() => setIsOpenDialogModif(true)}
+                    >
+                        Edit
+                    </Button>
+                </Box>
+                <Grid
+                    container
+                    spacing={7}
+                    direction="row"
+                    sx={{
+                        width: 'fit-content',
+                    }}
                 >
-                    Edit
-                </Button>
-            </Box>
-            <Grid
-                container
-                spacing={7}
-                direction="row"
-                sx={{
-                    width: 'fit-content',
-                }}
-            >
-                <Grid item>
-                    {staticData.map((element, index) => (
-                        <Typography
-                            key={index}
-                            variant="body2"
-                            paddingBottom='20px'
-                        >
-                            {formatMessage({ id: element })} :
-                        </Typography>
-                    ))}
-                </Grid>
-                <Grid item>
+                    <Grid item>
+                        {staticData.map((element, index) => (
+                            <Typography
+                                key={index}
+                                variant="body2"
+                                paddingBottom='20px'
+                            >
+                                {formatMessage({ id: element })} :
+                            </Typography>
+                        ))}
+                    </Grid>
+                    <Grid item>
                         {parishData && Object
                             .values(parishData)
                             .slice(1)
                             .map((value, index) => (
-                        <Typography
-                            variant="h5"
-                            color="var(--body)"
-                            paddingBottom='20px'
-                            key={index}
-                        >
-                            {value}
-                        </Typography>
-                    ))}
+                                <Typography
+                                    variant="h5"
+                                    color="var(--body)"
+                                    paddingBottom='20px'
+                                    key={index}
+                                >
+                                    {value}
+                                </Typography>
+                            ))}
+                    </Grid>
                 </Grid>
-            </Grid>
-        </Box >
+            </Box >
+            <MassesStatTable />
+        </>
     );
 }
 
