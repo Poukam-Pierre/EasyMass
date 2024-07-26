91ef985a6b04c508403f0d15b9b2162c44e8ac40
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-07-26 12:29:12 +0100

    fix(app):#31 Add case to assign css style.

--- apps/admin-ui/components/Finances/FinanceTable.tsx
+++ apps/admin-ui/components/Finances/FinanceTable.tsx
@@ -17,6 +17,77 @@ interface TableData {
     status: string;
     transactionId: string;
 }
+
+export const showTransactionStatus = (label: string) => {
+    switch (label) {
+        case 'done':
+            return (
+                <Typography sx={{
+                    bgcolor: 'rgba(92, 179, 96, 0.15)',
+                    color: 'var(--success)',
+                    width: 'fit-content',
+                    borderRadius: '20px',
+                    padding: 1,
+                    fontWeight: 'bold',
+                }}>
+                    {label}
+                </Typography>
+            )
+        case 'echec':
+            return (
+                <Typography sx={{
+                    bgcolor: 'rgba(199, 0, 0, 0.15)',
+                    color: 'var(--error)',
+                    width: 'fit-content',
+                    borderRadius: '20px',
+                    padding: 1,
+                    fontWeight: 'bold',
+                }}>
+                    {label}
+                </Typography>
+            )
+        case 'in process':
+            return (
+                <Typography sx={{
+                    bgcolor: 'rgba(156, 213, 245, 0.3)',
+                    color: 'var(--secondary)',
+                    width: 'fit-content',
+                    borderRadius: '20px',
+                    padding: 1,
+                    fontWeight: 'bold',
+                }}>
+                    {label}
+                </Typography>
+            )
+        case 'locked':
+            return (
+                <Typography sx={{
+                    bgcolor: 'rgba(255, 184, 0, 0.3)',
+                    color: 'var(--warning)',
+                    width: 'fit-content',
+                    borderRadius: '20px',
+                    padding: 1,
+                    fontWeight: 'bold',
+                }}>
+                    {label}
+                </Typography>
+            )
+        case 'open':
+            return (
+                <Typography sx={{
+                    bgcolor: 'rgba(2, 109, 169, 0.3)',
+                    color: 'var(--primary)',
+                    width: 'fit-content',
+                    borderRadius: '20px',
+                    padding: 1,
+                    fontWeight: 'bold',
+                }}>
+                    {label}
+                </Typography>
+            )
+    }
+}
+
 export default function FinanceTable() {
     const { formatNumber, formatMessage } = useIntl()
     const titles = ['amount', 'paymentMethod', 'date', 'status', 'transactionId']
@@ -27,7 +98,7 @@ export default function FinanceTable() {
             amount: 2000,
             paymentType: paymentType.mtn,
             date: '12-01-2023',
-            status: 'valide',
+            status: 'done',
             transactionId: 'JDUFL25461UBHS'
         },
         {
@@ -43,7 +114,7 @@ export default function FinanceTable() {
             amount: 2000,
             paymentType: paymentType.card,
             date: '12-01-2023',
-            status: 'valide',
+            status: 'in process',
             transactionId: 'JDUFL25461UBHS'
         },
     ]
@@ -53,36 +124,6 @@ export default function FinanceTable() {
         setDataTransactions(tableData)
     ), [])
 
-    const showTransactionStatus = (label: string) => {
-        switch (label) {
-            case 'valide':
-                return (
-                    <Typography sx={{
-                        bgcolor: 'rgba(92, 179, 96, 0.15)',
-                        color: 'var(--success)',
-                        width: 'fit-content',
-                        borderRadius: '20px',
-                        padding: 1,
-                        fontWeight: 'bold',
-                    }}>
-                        {label}
-                    </Typography>
-                )
-            case 'echec':
-                return (
-                    <Typography sx={{
-                        bgcolor: 'rgba(199, 0, 0, 0.15)',
-                        color: 'var(--error)',
-                        width: 'fit-content',
-                        borderRadius: '20px',
-                        padding: 1,
-                        fontWeight: 'bold',
-                    }}>
-                        {label}
-                    </Typography>
-                )
-        }
-    }
 
     return (
         <Table>
