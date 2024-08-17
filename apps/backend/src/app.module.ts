f760d3b03fe5f070da5e71f1081fb8b37e680368
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-08-17 12:04:14 +0100

    fix(app):#45 relocalization of file to src dir.

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
+import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, AuthModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
