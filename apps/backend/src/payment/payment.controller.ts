30ee19534d415a8a283b72e8ab5033b6db805824
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-08-19 14:25:48 +0100

    feat(app):#46 add guard and route the perform handler.

--- apps/backend/src/payment/payment.controller.ts
+++ apps/backend/src/payment/payment.controller.ts
@@ -1,10 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { PaymentService } from './payment.service';
+import { AuthGuard } from '../auth/guard/auth.guards';
+import { Public } from '../auth/decorator/public.decorator';
 
 @Controller('payment')
+@UseGuards(AuthGuard)
 export class PaymentController {
   constructor(private readonly paymentService: PaymentService) {}
 
+  @Public()
   @Post()
   handlePayment(
     @Body()
@@ -13,11 +17,19 @@ export class PaymentController {
       currency: string;
       channel: string;
       customer: {
+        name: string;
         phone: string;
       };
       metadata?: Array<object>;
     }
   ) {
     return this.paymentService.handlePayment(paymentInfos);
+  }
+
+  // TODO As this route is public, check if there is no scam good. Put it a new guard just for validate that.
+  @Public()
+  @Post('/notifications')
+  notifyPayment(@Body() paymentResult: object) {
+    return this.paymentService.notifyPayment(paymentResult);
   }
 }
