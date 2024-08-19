b8c920d61a2241d788604e54b8f95f425e7b311b
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-08-19 14:24:35 +0100

    feat(app):#46 import module to manage its service.

--- apps/backend/src/payment/payment.module.ts
+++ apps/backend/src/payment/payment.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
+import { BelieverModule } from '../believer/believer.module';
 
 @Module({
+  imports: [BelieverModule],
   providers: [PaymentService],
   exports: [PaymentService],
   controllers: [PaymentController],
