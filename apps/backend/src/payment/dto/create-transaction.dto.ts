c5bb7407174bc6487524c9ad234a3ae49af4da6f
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-08-20 07:07:58 +0100

    feat(app): #47 set up Dto for incoming data transaction

---
+++ apps/backend/src/payment/dto/create-transaction.dto.ts
@@ -0,0 +1,36 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateTransactionDto {
+  believerInfo: BelieverInfo;
+  massInfo: MassInfo[];
+  orderInfo: OrderInfo;
+}
+
+class BelieverInfo {
+  @IsString()
+  name?: string;
+  phone?: string;
+}
+
+class MassInfo {
+  @IsNumber()
+  @IsNotEmpty()
+  id: number;
+  price: number;
+
+  @IsString()
+  @IsNotEmpty()
+  parish: string;
+  dataTime: string;
+  intention: string;
+}
+
+class OrderInfo {
+  @IsNotEmpty()
+  @IsNumber()
+  amount: number;
+
+  @IsString()
+  @IsNotEmpty()
+  currency: string;
+}
