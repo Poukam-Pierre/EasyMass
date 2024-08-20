7d3d9c65dfabae95d73007012a566abe59535991
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-08-20 11:38:41 +0100

    feat(app):#51 export service for reusability and import prima to manage db.

--- apps/backend/src/notification-parish/notification-parish.module.ts
+++ apps/backend/src/notification-parish/notification-parish.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NotificationParishService } from './notification-parish.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [NotificationParishService],
+  exports: [NotificationParishService],
 })
 export class NotificationParishModule {}
