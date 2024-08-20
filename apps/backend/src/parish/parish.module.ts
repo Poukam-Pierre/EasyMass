fc604c07c0e67446e26ac9f4c96aa99674fd13cd
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-08-20 11:53:04 +0100

    feat(app):#49 import prisma to manage db and controller.

--- apps/backend/src/parish/parish.module.ts
+++ apps/backend/src/parish/parish.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ParishService } from './parish.service';
+import { ParishController } from './parish.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [ParishService],
+  controllers: [ParishController],
 })
 export class ParishModule {}
