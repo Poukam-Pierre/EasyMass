95c75092c5cd372ea3753416a4e3faa434098027
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-08-20 10:52:06 +0100

    feat(app):#54 add function to create or update table in db.

--- apps/backend/src/mass/mass.service.ts
+++ apps/backend/src/mass/mass.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
-export class MassService {}
+export class MassService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(createMassDto: Prisma.MassCreateInput) {
+    return this.prismaService.mass.create({
+      data: createMassDto,
+    });
+  }
+
+  async update(id: number, updateMassDto: Prisma.MassUpdateInput) {
+    return this.prismaService.mass.update({
+      where: { id },
+      data: updateMassDto,
+    });
+  }
+}
