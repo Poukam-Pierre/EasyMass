76e768a7b0ab9e093ba04b132ce33f6e58ddd224
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-08-20 11:23:10 +0100

    feat(app):#52 add create function to create an instance on the table.

--- apps/backend/src/notification/notification.service.ts
+++ apps/backend/src/notification/notification.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
-export class NotificationService {}
+export class NotificationService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(createNotifDto: Prisma.NotificationCreateInput) {
+    return this.prismaService.notification.create({
+      data: createNotifDto,
+    });
+  }
+}
