e1dfd5b78cd688f27f0e0678d03485fe0364c672
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-08-20 11:50:34 +0100

    feat(app):#50 add function to create instance on id.

--- apps/backend/src/notification-priest/notification-priest.service.ts
+++ apps/backend/src/notification-priest/notification-priest.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
-export class NotificationPriestService {}
+export class NotificationPriestService {
+  constructor(private primaService: PrismaService) {}
+
+  async create(createNotifPriestDto: Prisma.NotificationPriestCreateInput) {
+    return this.primaService.notificationPriest.create({
+      data: createNotifPriestDto,
+    });
+  }
+}
