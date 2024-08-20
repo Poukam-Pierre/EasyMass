e13d2338487d31277a651f84efc5f6a0b43ffbf8
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-08-20 09:43:32 +0100

    fix(app):#46 add prismo module to manage database table.

--- apps/backend/src/payment/payment.module.ts
+++ apps/backend/src/payment/payment.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
 import { BelieverModule } from '../believer/believer.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [BelieverModule],
+  imports: [BelieverModule, PrismaModule],
   providers: [PaymentService],
   exports: [PaymentService],
   controllers: [PaymentController],
