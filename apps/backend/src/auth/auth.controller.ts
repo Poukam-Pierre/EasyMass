90c360446958f742c92d216030998361b9a81de7
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-08-21 20:19:58 +0100

    feat(app):#56 add dto and specify login route.

--- apps/backend/src/auth/auth.controller.ts
+++ apps/backend/src/auth/auth.controller.ts
@@ -1,21 +1,29 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Public } from './decorator/public.decorator';
 import { AuthGuard } from './guard/auth.guards';
+import { LoginDataDto } from './dto/login.dto';
 
 @Controller('auth')
 @UseGuards(AuthGuard)
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @Post('/login-v1')
-  loginV1(@Body() input: { email: string; password: string }) {
+  @Post('/login-parish')
+  @Public()
+  loginParish(@Body(ValidationPipe) input: LoginDataDto) {
     return this.authService.authenticateParish(input);
   }
 
+  @Post('/login-admin')
   @Public()
-  @Post('/login-v2')
-  loginV2(@Body() input: { email: string; password: string }) {
+  loginAdmin(@Body(ValidationPipe) input: LoginDataDto) {
     return this.authService.authenticateAdmin(input);
   }
 }
