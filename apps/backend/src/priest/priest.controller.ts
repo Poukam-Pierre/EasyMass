4036147dabe424066d53ece5dd90c200d591dc69
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-08-21 12:35:20 +0100

    feat(app):#48 add routes , CRUD functions and guards.

--- apps/backend/src/priest/priest.controller.ts
+++ apps/backend/src/priest/priest.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { ROLE, Role } from '../auth/decorator/public.decorator';
+import { AdminGuard } from '../auth/guard/admin.guards';
+import { AuthGuard } from '../auth/guard/auth.guards';
+import { PriestService } from './priest.service';
 
-@Controller()
-export class PriestController {}
+@Controller('priest')
+export class PriestController {
+  constructor(private readonly priestService: PriestService) {}
+
+  create(@Body() createPriestDto: Prisma.PriestCreateInput) {
+    return this.priestService.create(createPriestDto);
+  }
+
+  @Get()
+  @UseGuards(AdminGuard)
+  @Role(ROLE.ADMIN)
+  @Role(ROLE.ENGENEER)
+  findAll() {
+    return this.priestService.findAll();
+  }
+
+  findOne(email: string) {
+    return this.priestService.findOne(email);
+  }
+
+  @Post(':id')
+  @UseGuards(AuthGuard)
+  update(id: string, updatePriestDto: Prisma.PriestUpdateInput) {
+    return this.priestService.update(+id, updatePriestDto);
+  }
+
+  @UseGuards(AuthGuard)
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.priestService.remove(+id);
+  }
+}
