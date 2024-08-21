19437aa3582a49be7140babdd632bbf13201c38f
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-08-21 11:38:42 +0100

    feat(app):#49 add CRUD functions with Guards into routes.

--- apps/backend/src/parish/parish.controller.ts
+++ apps/backend/src/parish/parish.controller.ts
@@ -1,4 +1,57 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { ParishService } from './parish.service';
+import { AdminGuard } from '../auth/guard/admin.guards';
+import { ROLE, Role } from '../auth/decorator/public.decorator';
 
-@Controller()
-export class ParishController {}
+@Controller('parishes')
+@UseGuards(AdminGuard)
+export class ParishController {
+  constructor(private readonly parishService: ParishService) {}
+
+  @Post()
+  @Role(ROLE.ADMIN)
+  create(@Body() createParishDto: Prisma.ParishCreateInput) {
+    return this.parishService.create(createParishDto);
+  }
+
+  @Get()
+  @Role(ROLE.ADMIN)
+  @Role(ROLE.ENGENEER)
+  findAll() {
+    return this.parishService.findAll();
+  }
+
+  @Get(':id')
+  @Role(ROLE.ADMIN)
+  @Role(ROLE.ENGENEER)
+  findOne(@Param('id') id: string) {
+    return this.parishService.findOne(+id);
+  }
+
+  @Patch(':id')
+  @Role(ROLE.ADMIN)
+  @UseGuards(AdminGuard)
+  update(
+    @Param('id') id: string,
+    @Body() updateParishDto: Prisma.ParishUpdateInput
+  ) {
+    return this.parishService.update(+id, updateParishDto);
+  }
+
+  @Role(ROLE.ADMIN)
+  @UseGuards(AdminGuard)
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.parishService.remove(+id);
+  }
+}
