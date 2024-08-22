12300fcc79fe604fe896c4140203f41c9b3f6ce6
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-08-22 05:44:49 +0100

    feat(app):#55 add prisma module to mange db and export administor service itseph.

--- apps/backend/src/administrator/administrator.module.ts
+++ apps/backend/src/administrator/administrator.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AdministratorService } from './administrator.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [AdministratorService],
+  exports: [AdministratorService],
 })
 export class AdministratorModule {}
