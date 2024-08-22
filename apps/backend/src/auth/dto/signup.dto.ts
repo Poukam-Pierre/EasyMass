b2cff02cbf776ae7aee94c57a9173c60e158dbe1
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-08-22 08:27:11 +0100

    feat(app):#58 set up sign up data for priest.

---
+++ apps/backend/src/auth/dto/signup.dto.ts
@@ -0,0 +1,12 @@
+export class SignUpDataDto {
+  firstName: string;
+  secondName: string;
+  image?: string;
+  birthDate: Date;
+  phone: string;
+  authNumber: number;
+  authCardImge: string;
+  availability: boolean;
+  email: string;
+  password: string;
+}
