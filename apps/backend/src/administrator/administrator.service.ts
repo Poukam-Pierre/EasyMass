01716c3044a09d460e66bf91c8e39ab01be3add0
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-08-22 05:45:20 +0100

    feat(app):#55 create CRUD functions

--- apps/backend/src/administrator/administrator.service.ts
+++ apps/backend/src/administrator/administrator.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
-export class AdministratorService {}
+export class AdministratorService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(createAdminDto: Prisma.AdministratorCreateInput) {
+    return this.prismaService.administrator.create({
+      data: createAdminDto,
+    });
+  }
+
+  async findAll() {
+    return this.prismaService.administrator.findMany();
+  }
+
+  async findOne(id: number) {
+    return this.prismaService.administrator.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async update(id: number, updateAdminDto: Prisma.AdministratorUpdateInput) {
+    return this.prismaService.administrator.update({
+      where: {
+        id,
+      },
+      data: updateAdminDto,
+    });
+  }
+
+  async remove(id: number) {
+    return this.prismaService.administrator.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
