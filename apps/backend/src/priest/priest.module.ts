fe47c6359cfeba551238f1aa5d5d0a8d0e85716c
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-08-24 02:02:10 +0100

    fix(app):#48 import prisma module instaed of Prisma service.

--- apps/backend/src/priest/priest.module.ts
+++ apps/backend/src/priest/priest.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { PriestService } from './priest.service';
+import { PrismaModule } from '../prisma/prisma.module';
 import { PriestController } from './priest.controller';
-import { PrismaService } from '../prisma/prisma.service';
+import { PriestService } from './priest.service';
 
 @Module({
-  imports: [PrismaService],
+  imports: [PrismaModule],
   providers: [PriestService],
   controllers: [PriestController],
   exports: [PriestService],
