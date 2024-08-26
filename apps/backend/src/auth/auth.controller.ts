425677fe6025bdad5d0478793db19378818a25f8
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-08-26 12:26:27 +0100

    fix(app): #44 remove authguard because all route is public.

--- apps/backend/src/auth/auth.controller.ts
+++ apps/backend/src/auth/auth.controller.ts
@@ -7,38 +7,32 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { Public, Role, ROLE } from './decorator/public.decorator';
-import { AuthGuard } from './guard/auth.guards';
+import { Role, ROLE } from './decorator/public.decorator';
 import { LoginDataDto } from './dto/login.dto';
-import { SignUpDataDto, SignUpParish } from './dto/signup.dto';
 import { RefreshToken } from './dto/refreshToken.dto';
+import { SignUpDataDto, SignUpParish } from './dto/signup.dto';
 import { AdminGuard } from './guard/admin.guards';
 
 @Controller('auth')
-@UseGuards(AuthGuard)
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/login-parish')
-  @Public()
   loginParish(@Body(ValidationPipe) input: LoginDataDto) {
     return this.authService.authenticate(input, 'parish');
   }
 
   @Post('/login-admin')
-  @Public()
   loginAdmin(@Body(ValidationPipe) input: LoginDataDto) {
     return this.authService.authenticate(input, 'admin');
   }
 
   @Post('/signup')
-  @Public()
   signUp(@Body(ValidationPipe) input: SignUpDataDto) {
     return this.authService.signup(input);
   }
 
   @Post('/singup-admin')
-  @Public()
   @UseGuards(AdminGuard)
   @Role(ROLE.ADMIN)
   signupAdmin() {
@@ -46,7 +40,6 @@ export class AuthController {
   }
 
   @Post('/singup-parish')
-  @Public()
   @UseGuards(AdminGuard)
   @Role(ROLE.ADMIN)
   @Role(ROLE.ENGENEER)
@@ -55,7 +48,6 @@ export class AuthController {
   }
 
   @Post('/refreshToken')
-  @Public()
   refreshToken(@Body(ValidationPipe) refreshToken: RefreshToken) {
     return this.authService.refreshToken(refreshToken);
   }
