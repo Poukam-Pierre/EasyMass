a8f19854a4abc98e3485bd438bf6bae57d5ff638
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-08-27 00:21:35 +0100

    fix(app):#44 adjust processing flow to get expected result.

--- apps/backend/src/auth/guard/admin.guards.ts
+++ apps/backend/src/auth/guard/admin.guards.ts
@@ -21,28 +21,34 @@ export class AdminGuard implements CanActivate {
       'role',
       context.getHandler()
     );
-
     if (!requiredRole) return false;
 
     const request = context.switchToHttp().getRequest();
-
-    const userRole: ROLE[] = request.user?.role; // Don't forget to set the user role during authentication.
-    if (!userRole || userRole.length === 0) return false;
-
     const token = this.extractTokenFromHeader(request);
-    if (!token) throw new UnauthorizedException();
+
+    if (!token) {
+      throw new UnauthorizedException();
+    }
 
     try {
       const payload = await this.JwtService.verifyAsync(token);
       request['user'] = {
         id: payload.id,
         email: payload.email,
+        role: payload.role,
       };
-
-      return userRole.some((role) => requiredRole.includes(role));
     } catch (error) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('Unauthorized action', {
+        cause: new Error(),
+        description: 'Invalid or expired token',
+      });
     }
+
+    const userRole: ROLE[] = [request.user.role];
+
+    if (!userRole || userRole.length === 0) return false;
+
+    return userRole.some((role) => requiredRole.includes(role));
   }
 
   private extractTokenFromHeader(request: Request): string | undefined {
