a1fa48927f90130409872736ba4073d49fa523f6
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-08-28 22:07:08 +0100

    feat(app):#65 add mass at the root of app.

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PaymentModule } from './payment/payment.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { MassModule } from './mass/mass.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, PaymentModule],
+  imports: [PrismaModule, AuthModule, PaymentModule, MassModule],
   controllers: [AppController],
   providers: [AppService],
 })
