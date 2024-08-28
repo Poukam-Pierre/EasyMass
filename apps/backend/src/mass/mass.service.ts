9f267ed898b9ae0c8752e45bf767c55f8e58bb17
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-08-28 19:53:09 +0100

    feat(app):#65 set up function to create masses for parishes.

--- apps/backend/src/mass/mass.service.ts
+++ apps/backend/src/mass/mass.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { Prisma } from '@prisma/client';
 import { CreateMassDto } from './dto/create-mass.dto';
@@ -7,8 +11,77 @@ import { CreateMassDto } from './dto/create-mass.dto';
 export class MassService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async createMasses(input: CreateMassDto, request) {
-    return;
+  /**
+   * This function is responsible to create masses from parish owner. It's take the data of mass and
+   * create them depending on the replicate boolean value. If it's true, then the function will create
+   * masses at the same time on the week until the end of the actual year. Otherwise it will create
+   * one mass at the specific time.
+   * @param input data of a mass to create
+   * @param request express request object that hold cretenrial parish data
+   * @returns
+   */
+  async createMasses(
+    input: CreateMassDto,
+    request
+  ): Promise<{ code: number; message: string }> {
+    const { id } = request.user;
+    const { replicate, processAt } = input;
+
+    const existingMass = await this.findAllByParish(id);
+
+    if (replicate) {
+      const allDateProcessMasses =
+        this.getDatesEvery7DaysUntilEndOfYear(processAt);
+
+      allDateProcessMasses.map(async (date) => {
+        const isMassAlreadyExists = existingMass.some(
+          (mass) => mass.processAt === date
+        );
+        if (isMassAlreadyExists) {
+          throw new ConflictException('Mass already exists', {
+            cause: new Error(),
+            description: `The mass ${date} is already created!`,
+          });
+        } else {
+          input.createByParish = {
+            connect: {
+              id: id,
+            },
+          };
+
+          input.processAt = date;
+          try {
+            await this.create(input);
+          } catch (error) {
+            throw new InternalServerErrorException();
+          }
+        }
+      });
+      return { code: 200, message: 'Masses created successfully!' };
+    }
+
+    const isMassAlreadyExists = existingMass.some(
+      (mass) => mass.processAt === processAt
+    );
+    if (isMassAlreadyExists) {
+      throw new ConflictException('Mass already exists', {
+        cause: new Error(),
+        description: 'Mass already created by the same parish',
+      });
+    }
+
+    input.createByParish = {
+      connect: {
+        id: id,
+      },
+    };
+
+    try {
+      await this.create(input);
+      return { code: 200, message: 'Mass created successfully!' };
+    } catch (error) {
+      throw new InternalServerErrorException();
+    }
   }
 
   async create(createMassDto: Prisma.MassCreateInput) {
