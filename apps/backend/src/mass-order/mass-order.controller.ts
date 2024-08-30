e6f9fe9ce9cd73f68cbf67ea8cc3e132ce1c85e6
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-08-30 21:34:39 +0100

    feat(app):#71 add route to get active mass order.

---
+++ apps/backend/src/mass-order/mass-order.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import { MassOrderService } from './mass-order.service';
+import { AuthGuard } from '../auth/guard/auth.guards';
+
+@Controller('/mass-order')
+export class MassOrderController {
+  constructor(private readonly massOrderService: MassOrderService) {}
+
+  @Get('/active')
+  @UseGuards(AuthGuard)
+  findAllActive(@Request() request) {
+    return this.massOrderService.findAllUnprocessMass(request);
+  }
+}
