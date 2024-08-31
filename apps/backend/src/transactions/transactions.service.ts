a4d22fcd94707a47ec23531293f0e5224d31f479
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-08-31 01:03:08 +0100

    feat(app):#73 set up function to retreive data transcations belongs to a specific parish.

--- apps/backend/src/transactions/transactions.service.ts
+++ apps/backend/src/transactions/transactions.service.ts
@@ -21,4 +21,22 @@ export class TransactionsService {
       data: updateTransactionDto,
     });
   }
+
+  async findAllTransactionByParish(id: number) {
+    return this.prismaService.transaction.findMany({
+      where: {
+        parishId: id,
+      },
+      select: {
+        id: true,
+        transactionId: true,
+        createdAt: true,
+        price: true,
+        status: true,
+        paymentMethod: true,
+        currency: true,
+        updatedAt: true,
+      },
+    });
+  }
 }
