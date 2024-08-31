7b1ecfb5ba88458d35f6fdbbfa8365bb24650198
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-08-31 01:01:56 +0100

    feat(app):#73 add module and controller on the root of App.

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -5,9 +5,18 @@ import { AuthModule } from './auth/auth.module';
 import { PaymentModule } from './payment/payment.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { MassModule } from './mass/mass.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { MassOrderModule } from './mass-order/mass-order.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, PaymentModule, MassModule],
+  imports: [
+    PrismaModule,
+    AuthModule,
+    PaymentModule,
+    MassModule,
+    TransactionsModule,
+    MassOrderModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
