db2a67d239bcaf24d5ca76af13ad5ab740beeb8b
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-09-04 12:44:42 +0100

    feat(app):#75 set up function to process ordering masses.

--- apps/backend/src/payment/payment.service.ts
+++ apps/backend/src/payment/payment.service.ts
@@ -3,12 +3,14 @@ import { createId } from '@paralleldrive/cuid2';
 import { ParishService } from '../parish/parish.service';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { TransactionsService } from '../transactions/transactions.service';
+import { BelieverService } from '../believer/believer.service';
 
 @Injectable()
 export class PaymentService {
   constructor(
     private readonly parishService: ParishService,
-    private readonly transactionsService: TransactionsService
+    private readonly transactionsService: TransactionsService,
+    private readonly believerService: BelieverService
   ) {}
 
   async handlePayment(handlePaymentDto: CreateTransactionDto) {
@@ -46,11 +48,19 @@ export class PaymentService {
     }
   }
 
+  /**
+   * This function handles the payment confirmation and then creates the believer owner.
+   * @param paymentResult
+   * @returns
+   */
   async notifyPayment(paymentResult) {
+    // Verify what data is received and extract metadata
     const {
-      data: { reference },
+      data: { reference, paymentMethod },
     } = paymentResult;
 
+    const { believerInfo, massInfo } = JSON.parse(paymentResult.data);
+    const believerId = createId();
     const checkPayment = {
       port: 443,
       method: 'GET',
@@ -64,7 +74,36 @@ export class PaymentService {
         checkPayment
       ).then((response) => response.json());
 
-      return paymentStatus;
+      // If payment status is positif, then save the believer owner in db.
+      // Then return the confirmation message for ordering masses.
+      if (paymentStatus) {
+        await this.believerService.create({
+          id: believerId,
+          name: believerInfo.name,
+          phone: believerInfo.phone,
+          massOrder: {
+            create: {
+              intension: massInfo.intension,
+              price: massInfo.price,
+              mass: {
+                connect: {
+                  id: massInfo.id,
+                },
+              },
+            },
+          },
+        });
+
+        // Extract all data from webhook parameters
+        await this.transactionsService.create({
+          transactionId: reference,
+          currency: 'xaf',
+          price: massInfo.price,
+          status: 'VALIDED',
+          paymentMethod: paymentMethod,
+        });
+      }
+      return 'Bill of masses ordered';
     } catch (error) {
       throw new UnprocessableEntityException(error);
     }
