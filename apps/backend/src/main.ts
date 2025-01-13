a583688386fe2a4d040a669c7c167ef9d4d53ff1
Poukam-Pierre <ngamaleu2011@gmail.com>
2025-01-13 12:50:24 +0100

    feat(app):#84 add CORS

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -8,6 +8,11 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    methods: 'GET, POST, PATCH, DELETE',
+    credentials: true,
+  });
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
