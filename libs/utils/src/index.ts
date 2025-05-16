3e405b0b73eac19ba83f18e9a7a4643c82c7808b
Poukam-Pierre <ngamaleu2011@gmail.com>
2025-05-16 15:40:32 +0100

    feat(app): #99 set up error handle function for managing error.

--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/phone';
 export * from './lib/utils';
 export * from './lib/apiMiddleware';
+export * from './lib/errorHandling';
---
+++ libs/utils/src/lib/errorHandling.ts
@@ -0,0 +1,53 @@
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+export function errorHandling({
+  error,
+  formatMessage,
+  redirect,
+}: {
+  error: unknown;
+  formatMessage: (message: { id: string }) => string;
+  redirect: (link: string) => void;
+}) {
+  if (axios.isAxiosError(error)) {
+    const status = error.response?.status;
+    const message = error.response?.data?.message || error.message;
+
+    if (status === 401) {
+      toast.error(formatMessage({ id: message ?? 'unauthorized' }));
+      redirect('/');
+    }
+    if (status === 409) {
+      toast.error(formatMessage({ id: message ?? 'conflict' }));
+    }
+    if (status === 500) {
+      toast.error(formatMessage({ id: message ?? 'serverError' }));
+      redirect('/');
+    }
+    // TODO: Check the rest of the status codes
+    if (status === 403) {
+      toast.error(formatMessage({ id: message ?? 'forbidden' }));
+      redirect('/');
+    }
+    if (status === 404) {
+      toast.error(formatMessage({ id: message ?? 'notFound' }));
+      redirect('/');
+    }
+    if (status === 400) {
+      toast.error(formatMessage({ id: message ?? 'badRequest' }));
+    }
+    if (status === 422) {
+      toast.error(formatMessage({ id: message ?? 'unprocessableEntity' }));
+    }
+    if (status === 429) {
+      toast.error(formatMessage({ id: message ?? 'tooManyRequests' }));
+    }
+    if (status === 503) {
+      toast.error(formatMessage({ id: message ?? 'serviceUnavailable' }));
+    }
+    if (status === 504) {
+      toast.error(formatMessage({ id: message ?? 'gatewayTimeout' }));
+    }
+  }
+}
