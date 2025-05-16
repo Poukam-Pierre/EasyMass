3d6ba79df418e528c54cb7a509a93d04eef0ea6d
Poukam-Pierre <ngamaleu2011@gmail.com>
2025-05-16 06:05:28 +0100

    feat(app): #97 Adding description on create parish function.

--- apps/backend/src/parish/parish.controller.ts
+++ apps/backend/src/parish/parish.controller.ts
@@ -9,14 +9,16 @@ import {
   Request,
   UseGuards,
 } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Prisma } from '@prisma/client';
 import { ROLE, Role } from '../auth/decorator/public.decorator';
 import { AdminGuard } from '../auth/guard/admin.guards';
 import { AuthGuard } from '../auth/guard/auth.guards';
-import { ParishService } from './parish.service';
 import { SignUpParishDto } from './dto/signupParish.dto';
+import { ParishService } from './parish.service';
 
 @Controller('parishes')
+@ApiTags('Parish')
 export class ParishController {
   constructor(private readonly parishService: ParishService) {}
 
@@ -38,7 +40,7 @@ export class ParishController {
   @Role(ROLE.ADMIN)
   @Role(ROLE.ENGENEER)
   findOne(@Param('id') id: string) {
-    return this.parishService.findOne(+id);
+    return this.parishService.findParish(+id);
   }
 
   @Patch(':id')
@@ -56,11 +58,37 @@ export class ParishController {
     return this.parishService.remove(+id);
   }
 
+  @ApiOperation({
+    summary: 'Create a new parish with the given data',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized. Token is missing or invalid.',
+  })
+  @ApiResponse({
+    status: 403,
+    description:
+      'Forbidden. You do not have permission to access this resource.',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Parish created successfully.',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad Request. Invalid input data.',
+  })
+  @ApiResponse({
+    status: 500,
+    description:
+      'Internal Server Error. An error occurred while processing the request.',
+  })
   @Post('/new')
-  @UseGuards(AdminGuard)
-  @Role(ROLE.ADMIN)
-  @Role(ROLE.ENGENEER)
+  // TODO: uncomment this when the admin data credentials is ready
+  // @UseGuards(AdminGuard)
+  // @Role(ROLE.ADMIN)
+  // @Role(ROLE.ENGENEER)
   create(@Body() input: SignUpParishDto, @Request() request) {
-    return this.parishService.signupParish(input, request);
+    return this.parishService.createParish(input, request);
   }
 }
