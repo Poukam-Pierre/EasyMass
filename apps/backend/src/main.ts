26a89c22ea84f29fc23cf61fa515d62fc187d7fd
Poukam-Pierre <ngamaleu2011@gmail.com>
2025-05-16 03:25:05 +0100

    feat(app) : #97 Set up swagger for API documentation

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,6 +3,8 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
@@ -13,6 +15,18 @@ async function bootstrap() {
     methods: 'GET, POST, PATCH, DELETE',
     credentials: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Easy Messe API')
+    .setDescription(
+      'The Easy Messe API is a RESTful API for managing church masses and related data.'
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
