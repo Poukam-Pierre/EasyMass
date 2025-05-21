cca57ca570b68ae8b7a5fb439d30297c6d6b88be
Poukam-Pierre <ngamaleu2011@gmail.com>
2025-05-21 14:45:15 +0100

    feat(app): #100 add API tags and improve response documentation for mass order retrieval

--- apps/backend/src/mass-order/mass-order.controller.ts
+++ apps/backend/src/mass-order/mass-order.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Query, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, Request, UseGuards } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AuthGuard } from '../auth/guard/auth.guards';
 import { MassOrderService } from './mass-order.service';
 
 @Controller('/mass-order')
+@ApiTags('mass-order')
 export class MassOrderController {
   constructor(private readonly massOrderService: MassOrderService) {}
 
@@ -12,9 +14,34 @@ export class MassOrderController {
     return this.massOrderService.findAllUnprocessMass(request);
   }
 
+  // @Get()
+  // @UseGuards(AuthGuard)
+  // findMassOrderByMass(@Query('massId') massId: string) {
+  //   return this.massOrderService.findMassOrderByMass(+massId);
+  // }
+
+  @ApiOperation({
+    summary: 'Find out all masses created by parish',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Requests retreive successfully',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal server error',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'No mass requested found',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorize exception',
+  })
   @Get()
   @UseGuards(AuthGuard)
-  findMassOrderByMass(@Query('massId') massId: string) {
-    return this.massOrderService.findMassOrderByMass(+massId);
+  getAllMassOrder(@Req() request) {
+    return this.massOrderService.findAllMassOrder(request);
   }
 }
