df29ff51ac73760a6f1fb4fb3b421c842f93833c
Poukam-Pierre <ngamaleu2011@gmail.com>
2025-05-22 20:53:24 +0100

    feat(app): #100 update CreateMassDto and Mass model to enforce required fields and improve data integrity

--- apps/backend/src/mass/dto/create-mass.dto.ts
+++ apps/backend/src/mass/dto/create-mass.dto.ts
@@ -1,15 +1,46 @@
-export class CreateMassDto {
-  price: number;
-  processAt: string;
-  massType: MassType;
-  replicate: boolean;
-  createdByParish: { connect: { id: number } };
-}
+import { ReplicationPeriodEnum } from '@easyMesseLibs/types';
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import {
+  IsBoolean,
+  IsDate,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+} from 'class-validator';
 
-enum MassType {
-  UNIQUE = 'UNIQUE',
-  TRIDUM = 'TRIDUM',
-  SEVEN = 'SEVEN',
-  NOVENA = 'NOVENA',
-  THIRTY = 'THIRTY',
+export class CreateMassDto {
+  @ApiProperty({
+    description: "mass's price",
+    example: `xaf${2500}`,
+  })
+  @IsNumber()
+  @IsNotEmpty()
+  price: number;
+
+  @ApiProperty({
+    description: 'Date when the mass will be proceed',
+    example: new Date(),
+  })
+  @Type(() => Date)
+  @IsDate()
+  @IsNotEmpty()
+  processAt: Date;
+
+  @ApiProperty({
+    description: 'Given if mass has to be replicate throught the time of not',
+    example: false,
+  })
+  @IsBoolean()
+  @IsNotEmpty()
+  canReplicate: boolean;
+
+  @ApiProperty({
+    description: 'Given period mass might be replicate',
+    example: 'YEARLY',
+  })
+  @IsEnum(ReplicationPeriodEnum, {
+    message: 'Provided value does not support',
+  })
+  period: ReplicationPeriodEnum;
 }
