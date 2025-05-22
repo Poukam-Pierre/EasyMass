2c6c08da980cc94b5244824e5bb09b7af6245670
Poukam-Pierre <ngamaleu2011@gmail.com>
2025-05-22 20:44:45 +0100

    feat(app): #100 reorganize imports and enhance create mass method with consistent guards and API documentation

--- apps/backend/src/mass/mass.controller.ts
+++ apps/backend/src/mass/mass.controller.ts
@@ -1,27 +1,53 @@
 import {
   Body,
   Controller,
-  Patch,
-  Post,
-  Request,
-  UseGuards,
-  Param,
   Delete,
   Get,
+  Param,
+  Patch,
+  Post,
   Query,
+  Req,
+  UseGuards,
 } from '@nestjs/common';
-import { MassService } from './mass.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Prisma } from '@prisma/client';
 import { AuthGuard } from '../auth/guard/auth.guards';
 import { CreateMassDto } from './dto/create-mass.dto';
-import { Prisma } from '@prisma/client';
+import { MassService } from './mass.service';
 
 @Controller('masses')
 @UseGuards(AuthGuard)
+@ApiTags('masses')
 export class MassController {
   constructor(private readonly massService: MassService) {}
 
+  @ApiOperation({
+    summary: 'Create masses',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Masses has been created successfully',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorize exception',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal server error',
+  })
+  @ApiResponse({
+    status: 409,
+    description: 'Mass exists already',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Missing/bad data request',
+  })
   @Post('/create')
-  create(@Request() request, @Body() input: CreateMassDto) {
+  @UseGuards(AuthGuard)
+  create(@Req() request, @Body() input: CreateMassDto) {
     return this.massService.createMasses(input, request);
   }
 
@@ -43,6 +69,6 @@ export class MassController {
     @Query('parishId') parishId: string,
     @Query('role') role?: 'HISTORY'
   ) {
-    return this.massService.findAllByParish(+parishId, role);
+    return this.massService.findAllMasses(+parishId, role);
   }
 }
