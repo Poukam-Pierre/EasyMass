3e950bda48476840f9f0d9486a2c072e2b26bcde
Poukam-Pierre <ngamaleu2011@gmail.com>
2025-07-14 18:37:40 +0100

    feat(app): Set up exception filters.

---
+++ apps/backend/src/types/express/index.d.ts
@@ -0,0 +1,16 @@
+import { User as Person } from '@prisma/client';
+
+declare global {
+  namespace Express {
+    interface User extends Person {
+      id: string;
+      is_active: boolean;
+    }
+
+    namespace Express {
+      interface Request {
+        user?: User;
+      }
+    }
+  }
+}
