9388d46fb71b589716eaa07c41a5dfb29b4c3941
Poukam-Pierre <ngamaleu2011@gmail.com>
2025-07-16 00:16:33 +0100

    fix(app): enhance express namespace

--- apps/backend/src/types/express/index.d.ts
+++ apps/backend/src/types/express/index.d.ts
@@ -1,16 +1,7 @@
-import { User as Person } from '@prisma/client';
-
 declare global {
   namespace Express {
-    interface User extends Person {
-      id: string;
-      is_active: boolean;
-    }
-
-    namespace Express {
-      interface Request {
-        user?: User;
-      }
+    interface Request {
+      user: User;
     }
   }
 }
